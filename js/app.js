79e82db70b7388c690a05145dad5427f878c5e46
turavinin <aturavinin@gmail.com>
2020-12-11 16:06:42 -0300

    start basic styling left aside

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ const sketchContainer = document.querySelector('#sketch-container');
 
 // Create divs in sketch container
 const createDivs = (size) => {
+  // Create a variable to store the total number of divs
+  let amountOfDivs = size * size;
   // Loop as many times as required divs
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < amountOfDivs; i++) {
     sketchContainer.innerHTML += `<div class="cell"></div>`;
   }
 };
 
-createDivs(400);
+createDivs(20);
