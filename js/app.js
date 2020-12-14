b4e9ec90e0602e240acd535c60d09e80d10a9731
turavinin <aturavinin@gmail.com>
2020-12-14 20:46:45 -0300

    paint on hover

--- js/app.js
+++ js/app.js
@@ -1,19 +1,64 @@
-/* ------------------------------ DOM VARIABLES ----------------------------- */
+/* --------------- DOM VARIABLES -------------- */
 
 const sketchContainer = document.querySelector('#sketch-container');
 const sliderRange = document.querySelector('#slider-range');
 const sliderTextOutput = document.querySelector('.size-value');
+const cell = document.querySelectorAll('.cell');
 
-/* ----------------------------- WINDOW ON LOAD ----------------------------- */
+/* -------------- WINDOW ON LOAD -------------- */
 
 // Default grid
 window.onload = () => {
   sliderTextOutput.innerHTML = `${sliderRange.value} x ${sliderRange.value}`;
   createDivs(sliderRange.value);
   changeGridParamenter(sliderRange.value);
+  cells = document.getElementsByClassName('cell');
 };
 
-/* ------------------------------ SLIDER OUTPUT / DIV CREATION ----------------------------- */
+/* ---------------- PAINT CELLS---------------- */
+
+// Create a variable to toggle the action of painting
+let togglePaint = true;
+
+// Target the dynamic cells (divs)
+document.addEventListener('click', (e) => {
+  // Check the corresponding target and the state of the toggle
+  if (e.target.classList == 'cell' && togglePaint == true) {
+    // Paint clicked cell
+    e.target.style.backgroundColor = 'red';
+    // Paint cells on hover
+    paintOnHover(true);
+    // Change the state of the toggle
+    togglePaint = false;
+  } else if (togglePaint == false) {
+    // Paint with the same color the cell when finish painting
+    e.target.style.backgroundColor = 'red';
+    // Remover the action of painting
+    paintOnHover(false);
+    // Change the state of the toggle
+    togglePaint = true;
+  }
+});
+
+// Paint on hover
+const paintOnHover = (trigger) => {
+  if (trigger == true) {
+    // Paint
+    document.addEventListener('mouseover', paint);
+  } else {
+    // No paint
+    document.removeEventListener('mouseover', paint);
+  }
+};
+
+// Paint one cell
+const paint = (e) => {
+  if (e.target.classList == 'cell') {
+    e.target.style.backgroundColor = `red`;
+  }
+};
+
+/* ------- SLIDER OUTPUT / DIV CREATION ------- */
 
 // Output the selected slider range in span
 sliderRange.oninput = function () {
@@ -27,7 +72,7 @@ sliderRange.onmouseup = function () {
   changeGridParamenter(this.value);
 };
 
-/* ------------------------------- CREATE / DELETE DIVS ------------------------------ */
+/* ----------- CREATE / DELETE DIVS ----------- */
 
 // Create the wanted amount of divs
 const createDivs = (oneSide) => {
@@ -50,7 +95,7 @@ const deleteAllDivs = () => {
   }
 };
 
-/* ----------------- CHANGE THE GRID PARAMETERS ON SLIDER OUTPUT ---------------- */
+/* -------------- GRID PARAMETERS ON SLIDER OUTPUT ------------- */
 
 const changeGridParamenter = (newParameter) => {
   sketchContainer.style.gridTemplateColumns = `repeat(${newParameter}, 1fr)`;
